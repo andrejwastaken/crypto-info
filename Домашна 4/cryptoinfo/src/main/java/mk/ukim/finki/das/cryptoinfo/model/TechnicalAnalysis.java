1fddcbabf079b22bedeec10595419313452e0f11
Filip Gavrilovski <filipgavrilovski28@gmail.com>
2025-12-25 20:54:38 +0100

    refactor backend to have a single model for technical analysis

---
+++ Домашна 4/cryptoinfo/src/main/java/mk/ukim/finki/das/cryptoinfo/model/TechnicalAnalysis.java
@@ -0,0 +1,70 @@
+package mk.ukim.finki.das.cryptoinfo.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "technical_analysis")
+public class TechnicalAnalysis {
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "date")
+    private LocalDate date;
+
+    @Column(name = "osc_rsi")
+    private Double rsi;
+
+    @Column(name = "osc_macd_line")
+    private Double macdLine;
+
+    @Column(name = "osc_macd_signal")
+    private Double macdSignal;
+
+    @Column(name = "osc_stoch_k")
+    private Double stochK;
+
+    @Column(name = "osc_stoch_d")
+    private Double stochD;
+
+    @Column(name = "osc_dmi_plus")
+    private Double dmiPlus;
+
+    @Column(name = "osc_dmi_minus")
+    private Double dmiMinus;
+
+    @Column(name = "osc_adx")
+    private Double adx;
+
+    @Column(name = "osc_cci")
+    private Double cci;
+
+    @Column(name = "symbol", length = Integer.MAX_VALUE)
+    private String symbol;
+
+    @Column(name = "ma_sma")
+    private Double sma;
+
+    @Column(name = "ma_ema")
+    private Double ema;
+
+    @Column(name = "ma_wma")
+    private Double wma;
+
+    @Column(name = "ma_bollinger_middle")
+    private Double bollingerMiddle;
+
+    @Column(name = "ma_volume_sma")
+    private Double volumeSma;
+
+    @Column(name = "normalized_score")
+    private Double normalizedScore;
+
+    @Column(name = "period")
+    @Enumerated(value = EnumType.STRING)
+    private TechnicalAnalysisTimePeriod period;
+
+
+}
--- Домашна 4/cryptoinfo/src/main/java/mk/ukim/finki/das/cryptoinfo/service/TechnicalAnalysisService.java
+++ Домашна 4/cryptoinfo/src/main/java/mk/ukim/finki/das/cryptoinfo/service/TechnicalAnalysisService.java
@@ -1,29 +1,18 @@
 package mk.ukim.finki.das.cryptoinfo.service;
 
+import lombok.AllArgsConstructor;
 import mk.ukim.finki.das.cryptoinfo.model.TechnicalAnalysisTimePeriod;
-import mk.ukim.finki.das.cryptoinfo.repository.TechnicalAnalysis1dRepository;
-import mk.ukim.finki.das.cryptoinfo.repository.TechnicalAnalysis1mRepository;
-import mk.ukim.finki.das.cryptoinfo.repository.TechnicalAnalysis1wRepository;
+import mk.ukim.finki.das.cryptoinfo.repository.TechnicalAnalysisRepository;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class TechnicalAnalysisService {
-    private final TechnicalAnalysis1dRepository repository1d;
-    private final TechnicalAnalysis1wRepository repository1w;
-    private final TechnicalAnalysis1mRepository repository1m;
+    private final TechnicalAnalysisRepository repository;
 
-    public TechnicalAnalysisService(TechnicalAnalysis1dRepository repository1d, TechnicalAnalysis1wRepository repository1w, TechnicalAnalysis1mRepository repository1m) {
-        this.repository1d = repository1d;
-        this.repository1w = repository1w;
-        this.repository1m = repository1m;
-    }
 
     public Double getScoreForSymbol(String symbol, TechnicalAnalysisTimePeriod timePeriod){
-        return switch (timePeriod) {
-            case DAY -> repository1d.findNormalizedScoreBySymbol(symbol);
-            case WEEK -> repository1w.findNormalizedScoreBySymbol(symbol);
-            case MONTH -> repository1m.findNormalizedScoreBySymbol(symbol);
-        };
+        return repository.findNormalizedScoreBySymbolAndPeriod(symbol, timePeriod);
     }
 
 }
